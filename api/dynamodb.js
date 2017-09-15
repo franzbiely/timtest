4cbb6ece47b3c62c8997aca933e88a73cb866a9d
Francis Albores <mrthemetribe@gmail.com>
2017-09-15 13:53:13 +0800

    /user route creating user

---
+++ api/dynamodb.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+module.exports = docClient;
--- api/user.js
+++ api/user.js
@@ -1,35 +1,60 @@
 'use strict';
+var AWS = require("aws-sdk");
 
-const uuid = require('uuid');
-const dynamodb = require('./dynamodb');
+AWS.config.update({
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+});
+
+
+
+
+
+module.exports.foo = (event, context, callback) => {
+    console.log(event);
+    const response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header": "My Header Value"
+        },
+        body: JSON.stringify(event.queryStringParameters)
+    };
+
+    callback(null, response);
+}
 
 module.exports.create = (event, context, callback) => {
-  const data = JSON.parse(event.body);
 
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Item: {
-      id: uuid.v1(),
-      email: data.email,
-      password: data.password,
-      createdAt: timestamp,
-      updatedAt: timestamp,
-    },
-  };
+	var docClient = new AWS.DynamoDB.DocumentClient();
+	
+	const data = event.queryStringParameters;
+	const params = {
+	    TableName: "Users",
+	    Item: {
+	        email: data.email,
+	        password: data.password
+	    },
+	};
+	
+	let response='';
 
-  dynamodb.put(params, (error) => {
-    let response;
+	docClient.put(params, function(err, data) {
+	    if (err) {
+	        response = {
+	            statusCode: 403,
+	            body: {
+	                status: "failed"
+	            }
+	        };
+	    } else {
+	        response = {
+	            statusCode: 200,
+	            body: {
+	                status: "success"
+	            }
+	        };
+	    }
+	    callback(null, response);
+	});
 
-    if (error) {
-        response = {
-	      status : "failed"
-	    };
-    }
-    else {
-	  	response = {
-	      status : "success"
-	    };	
-    } 
-    callback(null, response);
-  });
 };
